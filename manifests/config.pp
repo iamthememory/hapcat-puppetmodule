0c03ad12feea00b4eddc72f460fbe44def9cf879
Alex Corkwell <i.am.the.memory@gmail.com>
2018-03-14 15:00:39 -0400

    Add a service class

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,24 +7,6 @@
 # @example
 #   include hapcat::config
 class hapcat::config {
-  include systemd
-
-  if ! ($facts['systemd']) {
-    fail("hapcat only supports systemd systems currently")
-  }
-
-  file { $hapcat::service_file:
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0444',
-    content => epp('hapcat.service.epp', {
-      'service_user'             => $hapcat::service_user,
-      'service_group'            => $hapcat::service_group,
-      'service_workingdirectory' => $hapcat::service_workingdirectory,
-      'service_command'          => $hapcat::service_command,
-    },
-  }
 
   file { $hapcat::service_workingdirectory:
     ensure => directory,
@@ -32,4 +14,5 @@ class hapcat::config {
     group  => $hapcat::service_group,
     mode   => '0750',
   }
+
 }
---
+++ manifests/service.pp
@@ -0,0 +1,46 @@
+# hapcat::service
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include hapcat::service
+class hapcat::service {
+
+  if $hapcat::service_manage {
+
+    include systemd
+
+    if ! ($facts['systemd']) {
+      fail('hapcat only supports systemd systems currently')
+    }
+
+    if ! ($hapcat::service_ensure in [ 'running', 'stopped' ]) {
+      fail('service_ensure parameter must be running or stopped')
+    }
+
+    file { $hapcat::service_file:
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+      content => epp('hapcat.service.epp', {
+        'service_user'             => $hapcat::service_user,
+        'service_group'            => $hapcat::service_group,
+        'service_workingdirectory' => $hapcat::service_workingdirectory,
+        'service_command'          => $hapcat::service_command,
+      },
+      notify  => Class['systemd::systemctl::daemon_reload'],
+    }
+
+    service { 'hapcat':
+      ensure     => $hapcat::service_ensure,
+      enable     => $hapcat::service_enable,
+      name       => $hapcat::service_name,
+      subscribe  => File[$hapcat::service_file],
+    }
+
+  }
+
+}
