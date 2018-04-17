7edace8149bf3e9c73af044be34fa15ed834b80a
Alex Corkwell <i.am.the.memory@gmail.com>
2018-04-17 14:57:47 -0400

    Add a templated config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -15,4 +15,17 @@ class hapcat::config {
     mode   => '0750',
   }
 
+  file { $hapcat::package_config_file :
+    ensure  => file,
+    owner   => 'root',
+    group   => $hapcat::service_group,
+    mode    => '0440',
+    content => epp('hapcat/hapcatd.conf.epp', {
+      'api_address'           => $hapcat::api_address,
+      'api_port'              => $hapcat::api_port,
+      'database_url'          => $hapcat::database_url,
+      'database_loadtestdata' => $hacpat::database_loadtestdata,
+    }),
+  }
+
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,7 @@ class hapcat(
   $package_pip_url = 'git+https://github.com/iamthememory/hapcat-backend.git',
   $package_virtualenv = 'system',
   $package_pip_install_args = '',
+  Stdlib::Absolutepath $package_config_file = '/etc/hapcatd.conf',
 
   $service_command = '/usr/local/bin/hapcatd',
   Boolean $service_enable = true,
@@ -22,6 +23,11 @@ class hapcat(
   $service_name = 'hapcat',
   $service_user = 'hapcat',
   Stdlib::Absolutepath $service_workingdirectory = '/var/lib/hapcat',
+
+  Stdlib::Host $api_address = '0.0.0.0',
+  Stdlib::Port $api_port = 8080,
+  $database_url = "sqlite:////${hapcat::service_workingdirectory}/hapcatdb.sqlite3",
+  Boolean $database_loadtestdata = false,
 ) {
 
   contain hapcat::install
